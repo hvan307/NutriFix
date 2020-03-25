e8d9d0b9c66ca1a390ec6da146af46f13b4a2a61
lucymait <lucy.maitland@hotmail.com>
2020-03-25 10:06:06 +0000

    user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,30 @@
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const { secret } = require('../config/environment')
+
+function register(req, res) {
+  User
+    .create(req.body)
+    .then(user => {
+      res.status(201).send(user)
+    })
+    .catch(error => res.send(error))
+}
+
+function login(req, res) {
+  User
+    .findOne({ email: req.body.email })
+    .then(user => {
+      if (!user.validatePassword(req.body.password)) {
+        return res.status(401).send({ message: 'Unauthorized' })
+      }
+      const token = jwt.sign({ sub: user._id }, secret, { expiresIn: '12h' } )
+      res.status(202).send({ message: `Welcome back ${user.username}`, token })
+    })
+    .catch(error => res.send(error))
+}
+
+module.exports = {
+  register,
+  login
+}
