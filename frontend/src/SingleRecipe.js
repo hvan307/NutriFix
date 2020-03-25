109263790a92b55acc6216f674fe97c77807ca35
tombannister01 <tombannister2463@gmail.com>
2020-03-25 15:34:32 +0000

    single recipe

--- frontend/src/SingleRecipe.js
+++ frontend/src/SingleRecipe.js
@@ -6,12 +6,15 @@ class DisplaySingleRecipe extends React.Component {
   constructor() {
     super()
     this.state = {
-      recipes: {}
+      recipes: {
+        macronutrients: {}
+      }
     }
   }
 
   componentDidMount() {
-    axios.get('/api/recipe/:_id')
+    const id = this.props.match.params.id
+    axios.get(`/api/recipe/${id}`)
       .then(res => {
         this.setState({ recipes: res.data })
       })
@@ -22,21 +25,22 @@ class DisplaySingleRecipe extends React.Component {
     return <div className="tile is-ancestor">
       <div className="tile is-4 is-vertical is-parent">
         <div className="tile is-child box">
-          <p className="title">{recipe.recipeName}</p>
-          <p></p>
+          
+          <img src={recipe.image} />
+
           {console.log(recipe)}
         </div>
         <div className="tile is-child box">
-          <p className="title">Two</p>
-          <p></p>
+          <p className="title">Macros</p>
+          <p>Protein: {recipe.macronutrients.protein}</p>
+          <p>Carbohydrates: {recipe.macronutrients.carbohydrates}</p>
+          <p>Sugar: {recipe.macronutrients.sugars}</p>
         </div>
       </div>
       <div className="tile is-parent">
         <div className="tile is-child box">
-          <p className="title">Three</p>
-          <p></p>
-          <p></p>
-          <p></p>
+          <p className="title">Instructions</p>
+          <p>{recipe.instructions}</p>
         </div>
       </div>
     </div>
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import DisplayRecipes from './DisplayRecipes'
 import DisplaySingleRecipe from './SingleRecipe'
-
+import './style.scss'
 
 const App = () => {
   return <BrowserRouter>
     <Switch>
       <Route exact path="/recipes" component={DisplayRecipes} />
-      <Route exact path="/recipe/:_id" component={DisplaySingleRecipe} />
+      <Route exact path="/recipe/:id" component={DisplaySingleRecipe} />
     </Switch>
   </BrowserRouter>
 }
