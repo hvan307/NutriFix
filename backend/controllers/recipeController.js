5359272dba787934536f694b329d9e172b64022e
lucymait <lucy.maitland@hotmail.com>
2020-03-25 10:56:50 +0000

    recipe controller

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -6,9 +6,62 @@ function allRecipes(req, res) {
     .then(recipes => {
       res.send(recipes)
     })
-
 }
 
+function createRecipe(req, res) {
+  req.body.user = req.currentUser
+  Recipe
+    .create(req.body)
+    .then(recipe => {
+      res.status(201).send(recipe)
+    })
+}
+
+function singleRecipe(req, res) {
+  const id = req.params.id
+  Recipe
+    .findById(id)
+    .then(recipe => {
+      res.send(recipe)
+    })
+}
+
+function removeRecipe(req, res) {
+  const currentUser = req.currentUser
+  const id = req.params.id
+  Recipe 
+    .findById(id)
+    .then(recipe => {
+      if (!recipe.user.equals(currentUser._id)) return res.status(401).send({ message: 'Unauthorized' })
+      return recipe.remove()
+    })
+    .then(() => {
+      res.sendStatus(204)
+    })
+}
+
+function editRecipe(req, res) {
+  const currentUser = req.currentUser
+  const id = req.params.id
+  Recipe
+    .findById(id)
+    .then(recipe => {
+      if (!recipe.user.equals(currentUser._id)) return res.status(401).send({ message: 'Unauthorized' })
+      return recipe.set(req.body)
+    })
+    .then(recipe => {
+      return recipe.save()
+    })
+    .then(recipe => {
+      res.status(202).send(recipe)
+    })
+}
+
+
 module.exports = {
-  allRecipes
+  allRecipes,
+  createRecipe,
+  singleRecipe,
+  removeRecipe,
+  editRecipe
 }
