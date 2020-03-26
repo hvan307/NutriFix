f2781c8a9a426b55b6664d43fb7b184929ab21f6
tombannister01 <tombannister2463@gmail.com>
2020-03-26 15:53:25 +0000

    navbar

---
+++ frontend/src/MyRecipes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from './components/Hero'
+
+class MyRecipes extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+  render() {
+    return <Hero></Hero>
+  }
+
+
+
+}
+
+export default MyRecipes
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import DisplayRecipes from './DisplayRecipes'
 import DisplaySingleRecipe from './SingleRecipe'
 import './style.scss'
+import MyRecipes from './MyRecipes'
 
 const App = () => {
   return <BrowserRouter>
     <Switch>
-      <Route exact path="/recipes" component={DisplayRecipes} />
       <Route exact path="/recipe/:id" component={DisplaySingleRecipe} />
+      <Route exact path="/myrecipes" component={MyRecipes} />
+      <Route exact path="/recipes" component={DisplayRecipes} />
     </Switch>
   </BrowserRouter>
 }
