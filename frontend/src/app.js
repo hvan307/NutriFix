e9cb745b2f7be238d62bb187493b696b38eee3cd
lucymait <lucy.maitland@hotmail.com>
2020-03-26 16:12:32 +0000

    register

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import DisplayRecipes from './DisplayRecipes'
 import DisplaySingleRecipe from './SingleRecipe'
+import Register from './components/Register'
 import './style.scss'
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     <Switch>
       <Route exact path="/recipes" component={DisplayRecipes} />
       <Route exact path="/recipe/:id" component={DisplaySingleRecipe} />
+      <Route exact path="/register" component={Register}/>
     </Switch>
   </BrowserRouter>
 }
