ae6499a48dbffab2b85c5729166d2c5e85304b07
hvan307 <hongvan307@gmail.com>
2020-12-26 22:57:12 +0000

    changed dbURI

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -1,8 +1,7 @@
 const secret = 'This is a recipe secret with tom, fin, lucy and hanna'
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGODB_URI || 'mongodb+srv://heroku_q60ln0z9:Tusday12.@nutrifix-recipes.tscgj.mongodb.net/heroku_q60ln0z9?retryWrites=true&w=majority'
-
-// const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/recipe-db'
+// const dbURI = process.env.MONGODB_URI || 'mongodb+srv://heroku_q60ln0z9:Tusday12.@nutrifix-recipes.tscgj.mongodb.net/heroku_q60ln0z9?retryWrites=true&w=majority'
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/recipe-db'
 
 module.exports = {
   secret,
